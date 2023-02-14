dea82b72c04cb49956569f279eeb8ad1141e57b4
padac78 <72388269+padac78@users.noreply.github.com>
2023-02-14 12:25:24 +0100

    commit_GET

--- backend-Padac/api_backend.py
+++ backend-Padac/api_backend.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/api/v1', methods=['GET'])
-def hello_world():
-    return 'Test'
+@app.route('/api/v1/employees', methods=['GET'])
+def get_employees():
+    employees = [
+        {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
+        {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+        {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
+    ]
+    return jsonify(employees)
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8080)
+    app.run(debug=True, port=8080)
