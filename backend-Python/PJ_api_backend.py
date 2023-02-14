f9fd8f54e5b4ca8a89737edb8ef76759a8aff397
PierreJoDC <pierrejoaquimdacosta@gail.com>
2023-02-14 14:56:26 +0100

    on a vu un problem

--- backend-Python/PJ_api_backend.py
+++ backend-Python/PJ_api_backend.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/api/v1/employees', methods=['GET'])
-def get_employees():
-    employees = [
+employees = [
         {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
         {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
         {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
     ]
+@app.route('/api/v1/employees', methods=['GET'])
+def get_employees():
     return jsonify(employees)
 
     
@@ -20,14 +20,14 @@ def add_employee():
     return jsonify(new_employee), 201
 
 # Endpoint pour supprimer un employé par ID
-@app.route('/employees/<int:id>', methods=['DELETE'])
-def delete_employee(id):
-    global employees
-    for i, employee in enumerate(employees):
-        if employee['id'] == id:
-            del employees[i]
-            return 'Employé supprimé', 204
-    return 'Employé non trouvé', 404
+#@app.route('/employees/<int:id>', methods=['DELETE'])
+#def delete_employee(id):
+ #   global employees
+  ##  for i, employee in enumerate(employees):
+    #    if employee['id'] == id:
+     #       del employees[i]
+       #     return 'Employé supprimé', 204
+    #re#turn 'Employé non trouvé', 404
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+#if __name__ == '__main__':
+ #   app.run(debug=True, port=8080)
