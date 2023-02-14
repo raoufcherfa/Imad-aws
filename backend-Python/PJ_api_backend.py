c1851f21b24fa862d4cc44f502561d5fab3a51a7
PierreJoDC <pierrejoaquimdacosta@gail.com>
2023-02-14 15:00:17 +0100

    bon le post fonctionne git add PJ_api_backend.py !

--- backend-Python/PJ_api_backend.py
+++ backend-Python/PJ_api_backend.py
@@ -1,17 +1,17 @@
-from flask import Flask, jsonify, request 
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
 employees = [
-        {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
-        {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
-        {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
-    ]
+    {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
+    {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+    {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
+]
+
 @app.route('/api/v1/employees', methods=['GET'])
 def get_employees():
     return jsonify(employees)
 
-    
 @app.route('/api/v1/employees', methods=['POST'])
 def add_employee():
     new_employee = request.json
@@ -19,15 +19,14 @@ def add_employee():
     employees.append(new_employee)
     return jsonify(new_employee), 201
 
-# Endpoint pour supprimer un employé par ID
-#@app.route('/employees/<int:id>', methods=['DELETE'])
-#def delete_employee(id):
- #   global employees
-  ##  for i, employee in enumerate(employees):
-    #    if employee['id'] == id:
-     #       del employees[i]
-       #     return 'Employé supprimé', 204
-    #re#turn 'Employé non trouvé', 404
+@app.route('/employees/<int:id>', methods=['DELETE'])
+def delete_employee(id):
+    global employees
+    for i, employee in enumerate(employees):
+        if employee['id'] == id:
+            del employees[i]
+            return 'Employé supprimé', 204
+    return 'Employé non trouvé', 404
 
-#if __name__ == '__main__':
- #   app.run(debug=True, port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
