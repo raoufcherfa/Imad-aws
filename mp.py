807f2b814616fde4aef36a905f93436b264d0d4b
MathieuP91 <mathieu.perotti9127@gmail.com>
2023-02-14 12:49:39 +0100

    commit du python

---
+++ mp.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1/employees', methods=['GET'])
+def get_employees():
+    employees = [
+        {'id': 1, 'firstName': 'John', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'},
+        {'id': 2, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'},
+        {'id': 3, 'firstName': 'Bob', 'lastName': 'Johnson', 'emailId': 'bobjohnson@example.com'}
+    ]
+    return jsonify(employees)
+
+if __name__ == 'main':
+    app.run(debug=True, port=8080)
