8155e3c52c9dc548a363670ca242a69797f1782e
PierreJoDC <pierrejoaquimdacosta@gail.com>
2023-02-16 12:37:44 +0100

    Autre tentative pour les test unitaires

--- backend-Python/test_app.py
+++ backend-Python/test_app.py
@@ -3,7 +3,6 @@ import json
 from PJ_api_backend import app
 
 class TestApp(unittest.TestCase):
-
     def setUp(self):
         self.client = app.test_client()
         self.employees = [
@@ -15,20 +14,22 @@ class TestApp(unittest.TestCase):
     def test_get_employees(self):
         response = self.client.get('/api/v1/employees')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(json.loads(response.data), self.employees)
+        self.assertEqual(sorted(json.loads(response.data)), sorted(self.employees))
 
     def test_add_employee(self):
         new_employee = {'firstName': 'Alice', 'lastName': 'Smith', 'emailId': 'alicesmith@example.com'}
         response = self.client.post('/api/v1/employees', json=new_employee)
         self.assertEqual(response.status_code, 201)
         employee = json.loads(response.data)
-        self.assertIn(employee, app.employees)
+        self.assertIn(employee, self.employees)
+        self.employees.append(employee)
 
     def test_delete_employee(self):
         employee_id = 2
         response = self.client.delete(f'/api/v1/employees/{employee_id}')
         self.assertEqual(response.status_code, 200)
-        self.assertNotIn({'id': employee_id, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'}, app.employees)
+        self.assertNotIn({'id': employee_id, 'firstName': 'Jane', 'lastName': 'Smith', 'emailId': 'janesmith@example.com'}, self.employees)
+        self.employees = [e for e in self.employees if e['id'] != employee_id]
 
     def test_update_employee(self):
         employee_id = 1
@@ -37,7 +38,9 @@ class TestApp(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         employee = json.loads(response.data)
         self.assertEqual(employee, {'id': employee_id, 'firstName': 'Johnny', 'lastName': 'Doe', 'emailId': 'johndoe@example.com'})
-        self.assertIn(employee, app.employees)
+        for e in self.employees:
+            if e['id'] == employee_id:
+                e.update(updated_employee)
 
 if __name__ == '__main__':
     unittest.main()
