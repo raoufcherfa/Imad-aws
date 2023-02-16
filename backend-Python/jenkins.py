ae1f02e8bfbb5c6d541a230da498e11e3f733ff2
PierreJoDC <pierrejoaquimdacosta@gail.com>
2023-02-16 14:50:17 +0100

    Début Jenkins

---
+++ backend-Python/jenkins.py
@@ -0,0 +1,39 @@
+pipeline {
+    agent any
+    environment {
+        FLASK_APP = "PJ_api_backend.py"
+        FLASK_ENV = "development"
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([$class: 'GitSCM', branches: [[name: 'master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/raoufcherfa/employe.git%27]]])
+            }
+        }
+        stage('Build') {
+            steps {
+                sh 'pip install -r requirements.txt'
+            }
+        }
+        stage('Test') {
+            steps {
+                sh 'unittest unit_tests.py'
+            }
+        }
+        stage('Run API') {
+            steps {
+                sh 'python app.py &'
+            }
+        }
+        stage('Merge to Dev') {
+            steps {
+                sh 'git checkout Dev && git merge origin/master'
+            }
+        }
+        stage('Deploy to Dev') {
+            steps {
+                sh 'echo "http://localhost:5000/"'
+            }
+        }
+    }
+}
