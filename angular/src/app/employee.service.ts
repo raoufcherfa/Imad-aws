90fee9c5263fa9aa3d8bb1d76dea628d8be6ef0f
raoufcherfa <46077360+raoufcherfa@users.noreply.github.com>
2023-02-28 09:19:46 +0100

    Update employee.service.ts

--- angular/src/app/employee.service.ts
+++ angular/src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8080/api/v1/employees";
+  private baseURL = "http://localhost:8081/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
   
